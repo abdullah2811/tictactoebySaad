26aee0fa0a7fa9aeadd96bfe9a0561a91a4c2fbf
semisaad <sk515970@gmail.com>
2025-12-09 19:58:06 +0600

    Implementation of main game logic

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 let boxes = document.querySelectorAll('.box');
 let resetbtn = document.querySelector('#resetbtn');
+let turn = true;
 
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        if (box.innerText === '') 
+            {
+                box.innerText = turn ? 'X' : 'O';
+                turn = !turn;
+            }
+
+            checkWinner();
+        });
+});
+
+const checkWinner = () => {
+    winPatterns.forEach((pattern) =>{
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val !== '' && pos1val === pos2val && pos2val === pos3val) {
+            alert(`${pos1val} wins!`);
+            resetGame();
+        }
+    })
+}
